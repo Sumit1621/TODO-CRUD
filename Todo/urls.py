a91e3bb7e3d2de7946ca2f170e811af1068b277a
sourava kumar <souravakumar123@gmail.com>
2025-03-06 11:14:08 +0530

    'function_added'

--- Todo/urls.py
+++ Todo/urls.py
@@ -1,7 +1,10 @@
-# filepath: /D:/TODO-CRUD/Todo/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add/', views.add_task, name='add_task'),
+    path('toggle/<int:task_id>/', views.toggle_task, name='toggle_task'),
+    
 ]
+
