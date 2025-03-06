5627e246f8bcf8967020b36f2ba8986b86cf8203
sumit1621 <sumitpanda207@gmail.com>
2025-03-06 17:42:43 +0530

    upto date

--- Todo/urls.py
+++ Todo/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('add/', views.add_task, name='add_task'),
-    path('toggle/<int:task_id>/', views.toggle_task, name='toggle_task'),
+    path('', home, name='home'),
+    path('add/', add_task, name='add_task'),
+    path('toggle/<int:task_id>/', toggle_task, name='toggle_task'),
     
 ]
-
